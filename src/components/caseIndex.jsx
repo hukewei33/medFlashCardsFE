488e75e019d47b977103fd71117a6a346d29c4ed
Kenny Hu <hukewei33@gmail.com>
2021-07-07 17:31:20 +0800

    added exam type to case index

--- src/components/caseIndex.jsx
+++ src/components/caseIndex.jsx
@@ -5,16 +5,20 @@ import { Table} from 'react-bootstrap';
 export default function CaseIndex(){
     const { data, loading } = useFetch("http://127.0.0.1:8000/api/case-list/?format=json");
     if (data && ! loading ){
-        
+        if(data.length === 0 ){
+            return <>no cases in app</>
+        }
         return <Table>
         <thead>
             <tr>
                 <th>Case ID</th>
                 <th>Case Name</th>
+                <th>Exam Type</th>
+                
             </tr>
         </thead>
         <tbody>
-            {data.map(x=> <tr><td><a href={x.id}>{x.id}</a></td><td>{x.name}</td></tr>)}
+            {data.map(x=> <tr><td>{x.id}</td><td><a href={x.id}>{x.name}</a></td><td>{x.examtype.name}</td></tr>)}
         </tbody>
             
         </Table>
